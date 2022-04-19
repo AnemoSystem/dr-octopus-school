7acbb33d000d93007c39dd71b65d0139e67d4e54
ZegarekD <davivipe@hotmail.com>
2022-04-18 22:06:37 -0300

    Atualizei e modifiquei o minigame blockfall

---
+++ Assets/Scripts/Minigames/Blockfall/Ball.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody { get; private set; }
+    public float speed = 500f;
+
+    private void Awake()
+    {
+        this.rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        Invoke(nameof(SetRandomTrajectory), 1f);
+    }
+
+    private void SetRandomTrajectory()
+    {
+        Vector2 force = Vector2.zero;
+        force.x = Random.Range(-1f, 1f);
+        force.y = -1f;
+
+        this.rigidbody.AddForce(force.normalized * this.speed);
+    }
+}
--- Assets/Scripts/Minigames/Blockfall/Movimentation_blockfall.cs
+++ Assets/Scripts/Minigames/Blockfall/Movimentation_blockfall.cs
@@ -7,13 +7,18 @@ public class Movimentation_blockfall : MonoBehaviour
     private Vector2 targetPos;
     public float speed;
 
+    void Start()
+    {
+        targetPos = new Vector2(0, -8);
+    }
+
     void Update()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         if (Input.GetMouseButton(0))
         {
-            targetPos = new Vector2(mousePos.x, -4);
+            targetPos = new Vector2(mousePos.x, -8);
         }
 
         transform.position = Vector2.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
@@ -22,13 +27,13 @@ public class Movimentation_blockfall : MonoBehaviour
 
     void OnCollisionEnter2D()
     {
-        speed = 7f;
+        speed = 14f;
         transform.position = Vector2.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
     }
 
     void OnCollisionExit2D()
     {
-        speed = 7;
+        speed = 14;
         transform.position = Vector2.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
     }
 }

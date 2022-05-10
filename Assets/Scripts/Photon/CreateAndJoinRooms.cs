84a5f80f5663c4ef9dc735cdbf222a99b07608df
Guilherme Samuel <guisamuel800@gmail.com>
2022-05-09 22:07:57 -0300

    base multiplayer added

---
+++ Assets/Scripts/Photon/CreateAndJoinRooms.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
+{
+    public InputField createInput;
+    public InputField joinInput;
+    
+    public void CreateRoom() {
+        PhotonNetwork.CreateRoom(createInput.text);
+    }
+
+    public void JoinRoom() {
+        PhotonNetwork.JoinRoom(joinInput.text);
+    }
+
+    public override void OnJoinedRoom() {
+        PhotonNetwork.LoadLevel("MainMap");
+    }
+}

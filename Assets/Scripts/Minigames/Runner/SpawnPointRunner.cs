47efdd6825c2cd96daa76f6c0b60ac39e314b2ad
PlopesK <popi081004@hotmail.com>
2022-05-30 09:18:59 -0300

    Óia os sprites quentin hmmmm

--- Assets/Scripts/Minigames/Runner/SpawnPointRunner.cs
+++ Assets/Scripts/Minigames/Runner/SpawnPointRunner.cs
@@ -7,6 +7,7 @@ public class SpawnPointRunner : MonoBehaviour
     public GameObject obstacle;
 
     void Start() {
-        Instantiate(obstacle, transform.position, Quaternion.identity);    
+        Instantiate(obstacle, transform.position, Quaternion.identity); 
+        Destroy(gameObject,.3f);   
     }
 }

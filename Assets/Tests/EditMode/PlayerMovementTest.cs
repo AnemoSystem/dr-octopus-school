1fdbe0114e8e2290a6daa5dd0907942136ab12ef
Guilherme Samuel <guisamuel800@gmail.com>
2022-06-01 17:12:07 -0300

    setup unit test for player's movement

---
+++ Assets/Tests/EditMode/PlayerMovementTest.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+public class PlayerMovementTest
+{
+    [Test]
+    public void MoveAtPoint()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Assert.AreEqual();
+    }
+}

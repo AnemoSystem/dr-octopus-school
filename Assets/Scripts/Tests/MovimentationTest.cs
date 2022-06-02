ba5370dd6bdf3d547f573f151e85b2174be67d50
Guilherme Samuel <guisamuel800@gmail.com>
2022-06-02 00:34:32 -0300

    some unit tests

--- Assets/Scripts/Tests/MovimentationTest.cs
+++ Assets/Scripts/Tests/MovimentationTest.cs
@@ -8,35 +8,31 @@ public class MovimentationTest : MonoBehaviour
     private Vector2 targetPos;
     public float speed = 10;
 
-    void Start()
-    { 
-        //targetPos = new Vector2(0, -4);
-    }
-
-    void Update()
-    {
-
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        if (Input.GetMouseButton(0))
-        {
-            targetPos = new Vector3(mousePos.x, mousePos.y);
-        }
-
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
-
-        //transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPos);
+    void Update() {
+        Move(transform.position, targetPos);
     }
 
     void OnCollisionEnter2D()
     {
         speed = 0.1f;
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
     }
 
     void OnCollisionExit2D()
     {
         speed = 10;
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+    }
+
+    public void Move(Vector2 origin, Vector2 target) {
+        transform.position = Vector2.MoveTowards(origin, target, Time.deltaTime * speed);
+    }
+
+    public Vector3 getTargetPos() {
+        return targetPos;
+    }
+
+    public void setTargetPos(float v1, float v2) {
+        targetPos = new Vector2(v1, v2);
     }
 }

7401f443ce9c1a928601f7a9a798d501d563706b
Guilherme Samuel <guisamuel800@gmail.com>
2022-06-04 23:32:06 -0300

    player's sprite and animation (at map)

--- Assets/Scripts/Player/Movimentation.cs
+++ Assets/Scripts/Player/Movimentation.cs
@@ -6,8 +6,14 @@ using Photon.Pun;
 
 public class Movimentation : MonoBehaviour
 {
-    private Vector2 targetPos;
+    private Vector3 targetPos;
     public float speed = 10;
+    private Animator anim;
+
+    private Vector3 mousePos;
+    private bool isRunning = false;
+    private float rotationZ;
+    private Vector3 difference;
 
     PhotonView view;
 
@@ -15,22 +21,44 @@ public class Movimentation : MonoBehaviour
     { 
         //targetPos = new Vector2(0, -4);
         view = GetComponent<PhotonView>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Animation() {
+        isRunning = transform.position != targetPos;
+        
+        difference = mousePos - transform.position;
+        difference.Normalize();    
+        
+        if(rotationZ >= -45 && rotationZ <= 45)
+            anim.SetInteger("direction", 3);
+        else if(rotationZ > 45 && rotationZ <= 135)
+            anim.SetInteger("direction", 1);
+        else if(rotationZ < -45 && rotationZ >= -135)
+            anim.SetInteger("direction", 2);
+        else
+            anim.SetInteger("direction", 0);
+        
+        anim.SetBool("isRunning", isRunning);
     }
 
     void Update()
     {
         if(view.IsMine) {
 
-            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             if (Input.GetMouseButton(0))
             {
                 targetPos = new Vector3(mousePos.x, mousePos.y);
+                rotationZ = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
             }
 
             transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
 
             //transform.rotation = Quaternion.LookRotation(Vector3.forward, targetPos);
+
+            Animation();
         }
     }
 

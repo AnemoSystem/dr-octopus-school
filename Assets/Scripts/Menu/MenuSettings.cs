b68f452124c99ff738046feeea60ecd87617183e
capicoestar36 <101579225+capicoestar36@users.noreply.github.com>
2022-06-20 10:01:06 -0300

    resolutions options

--- Assets/Scripts/Menu/MenuSettings.cs
+++ Assets/Scripts/Menu/MenuSettings.cs
@@ -2,10 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+
 
 public class MenuSettings : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+    void Start ()
+    {
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            options.Add(option);
+        }
+        resolutionDropdown.AddOptions(options);
+    }
     public void SetVolume (float volume){
         audioMixer.SetFloat("volume", volume);
     }

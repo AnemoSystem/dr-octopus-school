a1dc22707612bbc5ab450fb1a1a965364e0dd3c4
Guilherme Samuel <guisamuel800@gmail.com>
2022-07-11 00:49:08 -0300

    adapting to student data

--- Assets/Scripts/MySQL/StudentData.cs
+++ Assets/Scripts/MySQL/StudentData.cs
@@ -10,15 +10,21 @@ public class StudentData : MonoBehaviour
     public Text teacherLabel;
     public Text subjectLabel;
     public Text[] gradesLabels;
+    public Text numAbsentLabel;
+    public Text numClassLabel;
+    public Text frequencyLabel;
 
-    public void SearchGrades() {
-        StartCoroutine(GetGrades());
+    private int id = 0; 
+    public string[] subjects;
+
+    public void SearchGrades(string subject) {
+        StartCoroutine(GetGrades(subject));
     }
 
-    IEnumerator GetGrades() {
+    IEnumerator GetGrades(string subject) {
         WWWForm form = new WWWForm();
         form.AddField("username", Server.username);
-        form.AddField("subject", "Matemática");
+        form.AddField("subject", subject);
 
         UnityWebRequest www = UnityWebRequest.Post("http://localhost/school-management-system/unity/grades.php", form);
         yield return www.SendWebRequest();
@@ -35,7 +41,7 @@ public class StudentData : MonoBehaviour
                 Debug.Log("HTTP Error");
                 break;
             case UnityWebRequest.Result.Success:
-                Debug.Log(www.downloadHandler.text);
+                //Debug.Log(www.downloadHandler.text);
                 string result = www.downloadHandler.text.ToString();
                 
                 string[] row = result.Split('#');
@@ -43,15 +49,31 @@ public class StudentData : MonoBehaviour
                 r.RemoveAt(row.Length - 1);
                 row = r.ToArray();
                 
-                Debug.Log("Length Row: " + row.Length);
+                //Debug.Log("Length Row: " + row.Length);
                 string[] data_values;
                 int month;
-                for(int i = 0; i < row.Length; i++) {
-                    data_values = row[i].Split('-');
-                    teacherLabel.text = "Professor: " + data_values[0];
-                    subjectLabel.text = "Matéria: " + data_values[1];
-                    month = Convert.ToInt32(data_values[3]);
-                    gradesLabels[month - 1].text = data_values[3] + "º Bimestre: " + data_values[2];
+                float freq;
+                for(int i = 1; i <= 4; i++) {
+                    if(i <= row.Length) {
+                        data_values = row[i - 1].Split('%');
+                        teacherLabel.text = "Professor: " + data_values[0];
+                        subjectLabel.text = "Matéria: " + data_values[1];
+                        month = Convert.ToInt32(data_values[3]);
+                        if(data_values[2] == "-1.0")
+                            gradesLabels[month - 1].text = data_values[3] + "º Bimestre: Nota não definida.";
+                        else
+                            gradesLabels[month - 1].text = data_values[3] + "º Bimestre: " + data_values[2];
+                        numClassLabel.text = "Aulas: " + data_values[4];
+                        numAbsentLabel.text = "Faltas: " + data_values[5];
+                        /*
+                        freq = Int16.Parse(data_values[5])/Int16.Parse(data_values[4]);
+                        Debug.Log(freq);
+                        frequencyLabel.text = "Frequência: " + Math.Round(freq, 2, MidpointRounding.ToEven).ToString();
+                        */
+                    }
+                    else {
+                        gradesLabels[i - 1].text = i.ToString() + "º Bimestre: Nota não definida.";
+                    }
                 }
                 break;
             default:
@@ -60,5 +82,19 @@ public class StudentData : MonoBehaviour
                 break;
         }
         www.Dispose();
+    }
+
+    public void NextOrPreviousSearch(bool next) {
+        if(next) id++;
+        else id--;
+        
+        if(id >= subjects.Length) id = 0;
+        else if(id < 0) id = subjects.Length - 1;
+        
+        StartCoroutine(GetGrades(subjects[id]));
+    }
+
+    void OnDisable() {
+        id = 0;
     }
 }

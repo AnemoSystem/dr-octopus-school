67e71b7a40b6a242e6bd5f490734baa6ee6b2f71
Guilherme Samuel <guisamuel800@gmail.com>
2022-07-20 16:13:37 -0300

    server & controller

--- Assets/Scripts/MySQL/Server.cs
+++ Assets/Scripts/MySQL/Server.cs
@@ -19,6 +19,7 @@ public class Server : MonoBehaviour
     public static int playersRoomA = 0;
     public static int playersRoomB = 0;
     public static bool canMove = true;
+    public static int bonusCoins = 0;
 
     public void Login() {
         StartCoroutine(Upload());

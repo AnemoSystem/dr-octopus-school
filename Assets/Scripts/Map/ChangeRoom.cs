7c96a249c3f1057e6b6c409d67bcd5026d398b05
Guilherme Samuel <guisamuel800@gmail.com>
2022-09-18 17:37:21 -0300

    change room, not level anymore

--- Assets/Scripts/Map/ChangeRoom.cs
+++ Assets/Scripts/Map/ChangeRoom.cs
@@ -2,20 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
 public class ChangeRoom : MonoBehaviourPunCallbacks
 {
     public LoadWithTransition transition;
     public string whichScene;
+    public string roomName;
+    public bool isIndividualScene = false;
 
     void OnTriggerEnter2D(Collider2D other) {
         //transition.FadeIn(whichScene);
-        PhotonNetwork.DestroyPlayerObjects(PhotonNetwork.player);
+        //PhotonNetwork.DestroyPlayerObjects(Server.username);
         PhotonNetwork.LeaveRoom();
         Debug.Log("encontrou");
     }
 
-    void OnLeftRoom() {
-        transition.FadeInRoom(whichScene);
+    public override void OnLeftRoom() {
+        if(isIndividualScene)
+            roomName += ("_" + Server.username);
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnJoinedLobby() {
+        PhotonNetwork.JoinOrCreateRoom(roomName, new RoomOptions() { MaxPlayers = 10 }, null);
+    }
+
+    public override void OnConnectedToMaster() {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedRoom() {
+        transition.FadeInLevel(whichScene);
     }
 }

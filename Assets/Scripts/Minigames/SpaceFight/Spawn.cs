6364d592ffbaf89c0e82b1bc68cef159f754d011
Guilherme Samuel <guisamuel800@gmail.com>
2022-09-26 10:01:26 -0300

    opa

--- Assets/Scripts/Minigames/SpaceFight/Spawn.cs
+++ Assets/Scripts/Minigames/SpaceFight/Spawn.cs
@@ -17,7 +17,7 @@ public class Spawn : MonoBehaviour
     void Spawner ( )
     {
         float spawnX = Random.Range
-            (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+            (Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x - 200);
         
         for(int i=0; i < waves; i++)
             Instantiate(enemies[(int)Random.Range(0,enemies.Length)],new Vector3(spawnX, Random.Range(7f,30f) ,0),Quaternion.identity);

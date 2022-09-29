83a26fd76414754d649bbc500cee61c27495c566
Guilherme Samuel <guisamuel800@gmail.com>
2022-09-29 07:57:32 -0300

    first dialogue system completed

--- Assets/Scripts/DialogueBox/FirstAccessDialogue.cs
+++ Assets/Scripts/DialogueBox/FirstAccessDialogue.cs
@@ -7,10 +7,11 @@ public class FirstAccessDialogue : MonoBehaviour
     public DialogueTrigger trigger;
     public Dialogue dialogue;
     public GameObject dialogueBoxObject;
+    public GameObject blackBK;
 
     void Start()
     {
-        if(Server.firstOpenning)    
+        if(Server.firstOpenning)
             StartCoroutine(PresentationGame());
         else
             trigger.gameObject.SetActive(false);
@@ -20,5 +21,20 @@ public class FirstAccessDialogue : MonoBehaviour
         yield return new WaitForSeconds(2f);
         dialogueBoxObject.SetActive(true);
         trigger.getManager().StartDialogue(dialogue);
+    }
+
+    public void StopDialogueBox() {
+        //StartCoroutine(Stopping());
+        
+        Destroy(trigger.gameObject);
+        Destroy(blackBK);
+        Destroy(trigger.getManager().gameObject);    
+    }
+
+    IEnumerator Stopping() {
+        yield return new WaitForSeconds(1f);
+        Destroy(trigger.gameObject);
+        Destroy(blackBK);
+        Destroy(trigger.getManager().gameObject);
     }
 }

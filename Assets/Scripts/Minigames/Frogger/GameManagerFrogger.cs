4980ba49e6e06f0c01aa8d31e12ec899817e01eb
capicoestar36 <101579225+capicoestar36@users.noreply.github.com>
2022-10-12 15:22:17 -0300

    first frogger implementation

---
+++ Assets/Scripts/Minigames/Frogger/GameManagerFrogger.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerFrogger : MonoBehaviour
+{
+    private Frogger frogger;
+    private Home[] homes;
+
+    public GameObject gameOverMenu;
+    public Text timeText;
+    public Text livesText;
+    public Text scoreText;
+
+    private int lives;
+    private int score;
+    private int time;
+
+    private float respawnTime;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+
+        homes = FindObjectsOfType<Home>();
+        frogger = FindObjectOfType<Frogger>();
+    }
+
+    private void Start()
+    {
+        NewGame();
+    }
+
+    private void NewGame()
+    {
+        gameOverMenu.SetActive(false);
+
+        SetScore(0);
+        SetLives(3);
+        NewLevel();
+    }
+
+    private void NewLevel()
+    {
+        for (int i = 0; i < homes.Length; i++) {
+            homes[i].enabled = false;
+        }
+
+        Respawn();
+    }
+
+    private void Respawn()
+    {
+        frogger.Respawn();
+
+        StopAllCoroutines();
+        StartCoroutine(Timer(30));
+    }
+
+    private IEnumerator Timer(int duration)
+    {
+        time = duration;
+        timeText.text = time.ToString();
+
+        while (time > 0)
+        {
+            yield return new WaitForSeconds(1);
+
+            time--;
+            timeText.text = time.ToString();
+        }
+
+        frogger.Death();
+    }
+
+    public void Died()
+    {
+        SetLives(lives - 1);
+
+        if (lives > 0) {
+            Invoke(nameof(Respawn), 1f);
+        } else {
+            Invoke(nameof(GameOver), 1f);
+        }
+    }
+
+    private void GameOver()
+    {
+        frogger.gameObject.SetActive(false);
+        gameOverMenu.SetActive(true);
+
+        StopAllCoroutines();
+        StartCoroutine(CheckForPlayAgain());
+    }
+
+    private IEnumerator CheckForPlayAgain()
+    {
+        bool playAgain = false;
+
+        while (!playAgain)
+        {
+            if (Input.GetKeyDown(KeyCode.Return)) {
+                playAgain = true;
+            }
+
+            yield return null;
+        }
+
+        NewGame();
+    }
+
+    public void AdvancedRow()
+    {
+        SetScore(score + 10);
+    }
+
+    public void HomeOccupied()
+    {
+        frogger.gameObject.SetActive(false);
+
+        int bonusPoints = time * 20;
+        SetScore(score + bonusPoints + 50);
+
+        if (Cleared())
+        {
+            SetLives(lives + 1);
+            SetScore(score + 1000);
+            Invoke(nameof(NewLevel), 1f);
+        }
+        else {
+            Invoke(nameof(Respawn), 1f);
+        }
+    }
+
+    private bool Cleared()
+    {
+        for (int i = 0; i < homes.Length; i++)
+        {
+            if (!homes[i].enabled) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void SetScore(int score)
+    {
+        this.score = score;
+        scoreText.text = score.ToString();
+    }
+
+    private void SetLives(int lives)
+    {
+        this.lives = lives;
+        livesText.text = lives.ToString();
+    }
+
+}

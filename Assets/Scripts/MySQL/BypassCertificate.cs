19f0ee8993d82685d087ee613085fbe7c2c6c4ad
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-31 22:32:59 -0300

    adding bypass certificate

---
+++ Assets/Scripts/MySQL/BypassCertificate.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BypassCertificate : CertificateHandler
+{
+    protected override bool ValidateCertificate(byte[] certificateData)
+    {
+        //Simply return true no matter what
+        return true;
+    }
+} 
